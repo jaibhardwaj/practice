563a539d839f17630f5f486bafe76f653d75f6db
Jai Krishan <jai.bhardwaj99@gmail.com>
2019-06-01 14:15:11 +0530

    Java Programm

--- javaprogrammes/src/main/java/com/practice/jai/ds/LinkedList1.java
+++ javaprogrammes/src/main/java/com/practice/jai/ds/LinkedList1.java
@@ -102,6 +102,28 @@ public class LinkedList1
             }
 
             current = current.getNextNode();
+
+
+            /*
+
+            HashSet<Integer> hs = new HashSet<>();
+
+            *//* Pick elements one by one *//*
+            node current = head;
+            node prev = null;
+            while (current != null)
+            {
+                int curval = current.val;
+
+                // If current value is seen before
+                if (hs.contains(curval)) {
+                    prev.next = current.next;
+                } else {
+                    hs.add(curval);
+                    prev = current;
+                }
+                current = current.next;
+            }*/
         }
 
         return this;
