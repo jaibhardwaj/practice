d089e94fe892b3d16c186a0a705eef4be09a9e3f
Jai Krishan <jai.bhardwaj99@gmail.com>
2019-06-01 15:17:58 +0530

    Xml Bean Factory

--- spring/src/main/java/com/jai/practice/basics/Triangle.java
+++ spring/src/main/java/com/jai/practice/basics/Triangle.java
@@ -2,10 +2,19 @@ package com.jai.practice.basics;
 
 public class Triangle implements Shape
 {
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     @Override
     public void draw()
     {
-        System.out.println("Triangle Drawn!");
+        System.out.println(getType() + " Triangle Drawn!");
     }
 }
